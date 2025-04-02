ef5d7ce28cfe58c6afe92ef51788bc37365ba752
@JELTech <jelt.tech@gmail.com>
2025-04-02 14:37:49 -0300

    Comando SP para separar una base de datos

---
+++ ScriptDB/SepararDBDETACH.sql
@@ -0,0 +1,6 @@
+-- Procedimiento para separar uan base de datos DETACH
+-- Procedimiento almacenado SP_DETACH_DB @DB_NAME=''
+
+-- Permite separar una base de datos del servidor actual
+SP_DETACH_DB @DBNAME='DBRESERVAII'
+GO
